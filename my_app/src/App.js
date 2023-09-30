5ab95b3e468f848292d539e95d75bf90c926ccbc
Emergin <ngangaephantus112@gmail.com>
2023-09-30 14:59:04 +0300

    updated

--- my_app/src/App.js
+++ my_app/src/App.js
@@ -1,40 +1,47 @@
+import React, { useState } from "react";
 import NewExpense from "./components/NewExpense/NewExpense";
+
 import Expenses from "./components/Expenses /Expenses";
+
+const DUMMY_EXPENSES = [
+  {
+    id: "e1",
+    title: "Toilet paper",
+    amount: 43.22,
+    date: new Date(2020, 2, 4),
+  },
+  {
+    id: "e2",
+    title: "new tv",
+    amount: 799.12,
+    date: new Date(2021, 4, 5),
+  },
+  {
+    id: "e3",
+    title: "car insurance",
+    amount: 123.45,
+    date: new Date(2022, 5, 6),
+  },
+  {
+    id: "e4",
+    title: "new Desk",
+    amount: 450,
+    date: new Date(2023, 6, 7),
+  },
+];
+
 function App() {
-  const addExpenseHandler = () => {
-    console.log("in app js");
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
+  const addExpenseHandler = (expense) => {
+    setExpenses((prevExpenses) => {
+      return [expense, ...expenses];
+    });
   };
-  const expenses = [
-    {
-      id: "e1",
-      title: "Toilet paper",
-      amount: 43.22,
-      date: new Date(2020, 2, 4),
-    },
-    {
-      id: "e2",
-      title: "new tv",
-      amount: 799.12,
-      date: new Date(2021, 4, 5),
-    },
-    {
-      id: "e3",
-      title: "car insurance",
-      amount: 123.45,
-      date: new Date(2022, 5, 6),
-    },
-    {
-      id: "e4",
-      title: "new Desk",
-      amount: 450,
-      date: new Date(2023, 6, 7),
-    },
-  ];
 
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
-      <Expenses items={expenses}/>
+      <Expenses items={expenses} />
     </div>
   );
 }
--- my_app/src/components/Expenses /Expenses.js
+++ my_app/src/components/Expenses /Expenses.js
@@ -1,31 +1,23 @@
 import Card from "../UI/Card";
+import React, { useState } from "react";
 import ExpenseItem from "./ExpenseItem";
-import './Expenses.css'
+import "./Expenses.css";
 function Expenses(props) {
-  
-  
+  let expensesContent = <p>No expenses found.</p>
+
+  if (props.items.length > 0) {
+    expensesContent = props.items.map((expense) => (
+      <ExpenseItem
+        key={expense.id}
+        title={expense.title}
+        amount={expense.amount}
+        date={expense.date}
+      />
+    ))
+  }
   return (
     <Card className="expenses">
-      <ExpenseItem
-        title={props.items[0].title}
-        amount={props.items[0].amount}
-        date={props.items[0].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[1].title}
-        amount={props.items[1].amount}
-        date={props.items[1].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[2].title}
-        amount={props.items[2].amount}
-        date={props.items[2].date}
-      ></ExpenseItem>
-      <ExpenseItem
-        title={props.items[3].title}
-        amount={props.items[3].amount}
-        date={props.items[3].date}
-      ></ExpenseItem>
+     {expensesContent}
     </Card>
   );
 }
